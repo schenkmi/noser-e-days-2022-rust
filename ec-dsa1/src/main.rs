18aee8496199ef1e2520e1920c4e6efc2aa34f74
Michael Schenk <michaelschenk72@gmail.com>
2022-08-25 16:05:10 +0200

    wip

--- ec-dsa1/src/main.rs
+++ ec-dsa1/src/main.rs
@@ -6,31 +6,18 @@
 // openssl ec -in private-key.pem -pubout -out public-key.pem
 //
 
-use std::env;
-
-use std::io::Write;
 use std::io::Read;
 use std::fs::File;
-//use std::fs::Metadata;
 use std::fs;
 use std::path::Path;
 
 use openssl::nid::Nid;
 use openssl::error::ErrorStack;
 use openssl::pkey::Private;
-use openssl::pkey::Public;
 use openssl::sign::{Signer, Verifier};
-use openssl::ec::{EcKey, EcGroup, EcPoint};
+use openssl::ec::{EcKey, EcGroup};
 use openssl::pkey::PKey;
 use openssl::hash::MessageDigest;
-
-
-fn get_ec_point() -> Result<EcPoint, ErrorStack> {
-   let group = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1)?;
-   let point = EcPoint::new(&group)?;
-   Ok(point)
-}
-
 
 fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
     let mut f = File::open(&filename).expect("no file found");
@@ -59,7 +46,6 @@ fn create_ec_keypair() {
     fs::write("private.pem", s).expect("Unable to write file");
 }
 
-
 fn get_ec_keypair() -> Result<EcKey<Private>, ErrorStack> {
     if !Path::new("private.pem").exists() {
         println!("private.pem not exist, creating it");
@@ -69,13 +55,6 @@ fn get_ec_keypair() -> Result<EcKey<Private>, ErrorStack> {
     // private PEM contains public and private key
     let data = fs::read("private.pem").expect("Unable to read file");
 
-
-    // create an EcKey from the binary form of a EcPoint
-    //let group = EcGroup::from_curve_name(Nid::SECP256K1).unwrap();
-
-    //let point = EcPoint::from_bytes(&group, &data, &mut ctx).unwrap();
-    //let key = EcKey::private_key_from_pem(&group, &point);
-
     // convert Vec<u8> to &[u8]
     let c = &data[..];
 
@@ -84,110 +63,28 @@ fn get_ec_keypair() -> Result<EcKey<Private>, ErrorStack> {
     return keypair;
 }
 
-
-
 fn main() {
-    println!("Hello, world!");
-    if let Ok(v) = env::var("DEP_OPENSSL_VERSION_NUMBER") {
-        let version = u64::from_str_radix(&v, 16).unwrap();
-
-        if version >= 0x1_01_01_00_0 {
-            println!("cargo:rustc-cfg=openssl111");
-        }
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() > 1 {
+        // &args[1] borrow
+        let filename = &args[1];
+        let filebin = get_file_as_byte_vec(filename);
+        println!("file data: {:?}", filebin);
     }
-
-
-    // ec key
-    let group = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1).unwrap();
-    let keypair = EcKey::generate(&group).unwrap();
-    
-    // no API to get pubkey
-    //let pubkey: PKey<Public> = PKey::from_ec_key(keypair).unwrap();
-    
-  
-
-    let pub_key = keypair.public_key_to_pem().unwrap();
-     // convert pem vector to string
-     let s = String::from_utf8(pub_key).expect("Found invalid UTF-8");
-
-     println!("public key = {:?}", s);
-
-    //println!("{:?}", str::from_utf8(pub_key.as_slice()).unwrap());
-
-
-    let keypair = PKey::from_ec_key(keypair).unwrap();
-
-
 
     // data to sign
     let data = b"hello, world!";
 
-    // hash: sha-256
-    let mut signer = Signer::new(MessageDigest::sha256(), &keypair).unwrap();
-    let buf_size = signer.len().unwrap();  // Computes an upper bound on the signature length.
-    println!("buffer size {}", buf_size);  // 72
-    let mut buf: [u8; 72] = [0; 72];
-
-    // sign
-    let exact_bytes = signer.sign_oneshot(&mut buf, data).unwrap(); //the number of bytes written.
-    println!("{}", exact_bytes); // 70
-
-
-    // Sign the data
-    let mut signer = Signer::new(MessageDigest::sha512(), &keypair).unwrap();
-    signer.update(data).unwrap();
-    let signature = signer.sign_to_vec().unwrap();
-
-    println!("ECDSA signatur: {:?}", signature);
-    
-    println!("Hello, world!");
-    //let data = b"hello, world!";
-
-    // Verify the data
-    let mut verifier = Verifier::new(MessageDigest::sha512(), &keypair).unwrap();
-    verifier.update(data).unwrap();
-
-    let result = verifier.verify(&signature).unwrap();
-
-    if result {
-        println!("verify OK :-)");
-    }
-    else {
-        println!("verify KO :-(");
-    }
-
-    // dump args
-    //let command_line: std::env::Args = std::env::args(); 
-
-    let args: Vec<_> = std::env::args().collect();
-
-    if args.len() > 1 {
-        // &args[1] borrow
-        let filename = &args[1];
-
-        let filebin = get_file_as_byte_vec(filename);
-
-        println!("file data: {:?}", filebin);
-    }
-
-    for argument in args {
-        println!("[{}]", argument);
-    }
-
-
-
+    // get or create ec keypair
     let ec_key_pair = get_ec_keypair().unwrap();
-
-
-    // Sign the data
     let private_key = PKey::from_ec_key(ec_key_pair).unwrap();
 
+    // Sign the data
     let mut signer = Signer::new(MessageDigest::sha512(), &private_key).unwrap();
     signer.update(data).unwrap();
     let signature = signer.sign_to_vec().unwrap();
 
     println!("ECDSA signatur: {:?}", signature);
-
 
     // Verify the data
     let mut verifier = Verifier::new(MessageDigest::sha512(), &private_key).unwrap();
